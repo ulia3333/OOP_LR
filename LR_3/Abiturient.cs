7b04f797273810138671bb0c1afbd64a24f5fa73
ulia3333 <90210056+ulia3333@users.noreply.github.com>
2021-11-15 19:53:04 +0300

    LR_3

---
+++ LR_3/Abiturient.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR_3
+{
+    public partial class Abiturient
+    {
+
+        // поля класса
+        private readonly int id;
+        private string surname;
+        private string firstName;
+        private string middleName;
+        private string addres;
+        private uint telNumber;
+        private int[] marks = new int[4];
+        static string allAbiturients;
+        static int abiCount;
+
+        public int ID => id;
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string MiddleName
+        {
+            get { return middleName; }
+            set { middleName = value; }
+        }
+
+        public string Addres
+        {
+            get { return addres; }
+            set { addres = value; }
+        }
+
+        public uint TelNumber
+        {
+            get { return telNumber; }
+            set { telNumber = value; }
+        }
+
+        public int Marks0
+        {
+            get => marks[0];
+            set
+            {
+
+                do
+                {
+                    if (value < 0)
+                        Console.WriteLine("Баллы не могут быть меньше нуля!");
+                    else
+                        marks[0] = value;
+                } while (value < 0);
+            }
+        }
+
+        public int Marks1
+        {
+            get => marks[1];
+            set
+            {
+
+                do
+                {
+                    if (value < 0)
+                        Console.WriteLine("Баллы не могут быть меньше нуля!");
+                    else
+                        marks[1] = value;
+                } while (value < 0);
+            }
+        }
+
+        public int Marks2
+        {
+            get => marks[2];
+            set
+            {
+
+                do
+                {
+                    if (value < 0)
+                        Console.WriteLine("Баллы не могут быть меньше нуля!");
+                    else
+                        marks[2] = value;
+                } while (value < 0);
+            }
+        }
+
+        public int Marks3
+        {
+            get => marks[3];
+            set
+            {
+
+                do
+                {
+                    if (value < 0)
+                        Console.WriteLine("Баллы не могут быть меньше нуля!");
+                    else
+                        marks[3] = value;
+                } while (value < 0);
+            }
+        }
+
+        // методы 
+
+        public void Method()
+        {
+            Console.WriteLine("Начало выполнения методов");
+
+        }
+
+        public static void info()
+        {
+            Console.WriteLine($"Всего абитуриентов: {abiCount}");
+            Console.WriteLine($"Абитуриент - {allAbiturients}");
+        }
+
+        public override int GetHashCode()
+        {
+            Console.WriteLine("Переопределение метода GetHashCode");
+            return base.GetHashCode();
+        }
+
+        public static Abiturient defConstructor()  //метод, вызывающий закрытый конструктор
+        {
+            return new Abiturient();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Abiturient objectType)
+            {
+                if (this.firstName == objectType.firstName && this.middleName == objectType.middleName
+                    && this.surname == objectType.surname && this.telNumber == objectType.telNumber
+                    && this.addres == objectType.addres && this.firstName == objectType.firstName
+                    && this.id == objectType.id && this.marks[0] == objectType.marks[0]
+                    && this.marks[1] == objectType.marks[1] && this.marks[2] == objectType.marks[2]
+                    && this.marks[3] == objectType.marks[3])
+                    return true;
+
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"Фамилия - {surname}, Имя - {firstName}, Отчество - {middleName}, Адрес - {addres}," +
+                   $" Номер тел. - {telNumber}, Баллы: {marks[0]}, {marks[1]}, {marks[2]}, {marks[3]}";
+        }
+
+        public int Sred()
+        {
+            return (marks[0] + marks[1] + marks[2] + marks[3]) / 4;
+        }
+
+        public int Max()
+        {
+            int max = 0;
+            for (int n = 0; n < 4; n++)
+            {
+                if (marks[n] >= max)
+                    max = marks[n];
+            }
+
+            return max;
+        }
+
+        public int Min()
+        {
+            int min = 101;
+            for (int n = 0; n < 4; n++)
+            {
+                if (marks[n] <= min)
+                    min = marks[n];
+            }
+
+            return min;
+        }
+
+        public int Sum()
+        {
+            return marks[0] + marks[1] + marks[2] + marks[3];
+        }
+
+
+        // конструкторы
+        static Abiturient()
+        {
+            allAbiturients = "Абитуриенты";
+            Console.WriteLine("Занесены данные нового абитуриента");
+        }
+
+        private Abiturient()
+        {
+            surname = "Пляшкевич";
+            firstName = "Кирилл";
+            middleName = "Викторович";
+            addres = "г. Минск";
+            telNumber = 8888;
+            marks[0] = 56;
+            marks[1] = 78;
+            marks[2] = 68;
+            marks[3] = 87;
+            id = GetHashCode();
+            abiCount++;
+        }
+
+        public Abiturient(string sn, string fn, string mn, string ad, uint tn, int ma1, int ma2, int ma3, int ma4)  // конструктор с параметрами по умолчанию
+        {
+            surname = sn;
+            firstName = fn;
+            middleName = mn;
+            addres = ad;
+            telNumber = tn;
+            marks[0] = ma1;
+            marks[1] = ma2;
+            marks[2] = ma3;
+            marks[3] = ma4;
+            id = HashCode.Combine(sn, mn, fn, ad, tn);
+            abiCount++;
+        }
+
+        public Abiturient(string sn, string fn, string mn, string ad, int ma1 = 50, int ma2 = 50, int ma3 = 50, int ma4 = 50)  // конструктор с параметрами по умолчанию
+        {
+            surname = sn;
+            firstName = fn;
+            middleName = mn;
+            addres = ad;
+            id = GetHashCode();
+            abiCount++;
+        }
+    }
+}
---
+++ LR_3/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace LR_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Abiturient abi1 = Abiturient.defConstructor();
+            Console.WriteLine(abi1.ToString());
+            Console.WriteLine($"Средний балл абитуриента составляет: {abi1.Sred()}");
+            Console.WriteLine(new string('=', 25));
+
+            Abiturient abi2 = new Abiturient("Худякова", "Анастасия", "Сергеевна", "г. Витебск", 8989, 45, 56, 46, 66); ;
+            Console.WriteLine($"Тип объекта abi2: {abi2.GetType()}");
+            abi2.TelNumber = 7878;
+            Console.WriteLine($"Мобильный номер телефна абитуриентки {abi2.Surname} {abi2.FirstName}: {abi2.TelNumber}");
+            Console.WriteLine($"Средний балл абитуриента составляет: {abi2.Sred()}");
+            Console.WriteLine(new string('=', 25));
+
+            Abiturient abi3 = new Abiturient("Петров", "Пёрт", "Петрович", "г. Могилёв");
+            abi3.Marks0 = 17;
+            abi3.Marks1 = 99;
+            abi3.Marks2 = 66;
+            abi3.Marks3 = 28;
+            Console.WriteLine($"Изменённый балл marks[0] равен: {abi3.Marks0}");
+            Console.WriteLine($"Средний балл абитуриента составляет: {abi3.Sred()}");
+            Console.WriteLine($"Минимальный балл абитуриента: {abi3.Min()}. Максимальный балл: {abi3.Max()}");
+
+            Console.WriteLine(new string('=', 25));
+            Console.WriteLine(abi1.Equals(abi3) ? "Объекты равны" : "Объекты не равны");
+            abi3.Method();
+           
+
+            Abiturient.info();
+            Console.WriteLine(new string('=', 25));
+
+            Abiturient[] abiturients = new Abiturient[3] { abi1, abi2, abi3 };
+
+            // 2 пункт задания
+            int k = 1;
+            bool flag = false;
+            Console.WriteLine("Введите минимальный балл: ");
+            int minEx = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Абитуриенты с баллами ниже заданного: ");
+            foreach (Abiturient abiturient in abiturients)
+            {
+                if (abiturient.Marks0 <= minEx || abiturient.Marks1 <= minEx ||
+                    abiturient.Marks2 <= minEx || abiturient.Marks3 <= minEx)
+                {
+                    Console.WriteLine($"{k}. {abiturient.Surname} {abiturient.FirstName}");
+                    k++;
+                    flag = true;
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine($"Абитуриентов с баллами ниже {minEx} нет.");
+            }
+
+            Console.WriteLine(new string('=', 25));
+
+            // 3 пункт задания
+            k = 1;
+            flag = false;
+            Console.WriteLine("Введите средний балл: ");
+            int sredMark = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Абитуриенты со средним баллом ниже заданного: ");
+            foreach (Abiturient abiturient in abiturients)
+            {
+                if (abiturient.Sred() > sredMark)
+                {
+                    Console.WriteLine($"{k}. {abiturient.Surname} {abiturient.FirstName}");
+                    k++;
+                    flag = true;
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine($"Абитуриентов со средним баллом выше {sredMark} нет.");
+            }
+
+            // 4) Анонимный тип
+            Console.WriteLine(new string('=', 25));
+            var newAbiturient = new { surname = "Ермолович", firstName = "Леонид", middleName = "Дмитриевич", addres = "г. Минск", telNumber = 8888 };
+            Console.WriteLine($"Тип объекта newAbiturient: {newAbiturient.GetType()}");
+            Console.WriteLine("Данные о абитуриенте");
+            Console.WriteLine($"Фамилия: {newAbiturient.surname} Имя: {newAbiturient.firstName} Отчество: {newAbiturient.middleName} " +
+                $"Адрес: {newAbiturient.addres} Номер телефона: {newAbiturient.telNumber}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
